6172aee61a09ffcf296c7cd1c709a9052a888e1a
10zin2002 <134126508+10zin2002@users.noreply.github.com>
2023-05-20 12:02:31 +0530

    Update DisjointSet.cpp
    By size

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -1,8 +1,8 @@
 class DisjointSet {
-    vector<int> rank, parent;
+    vector<int> size, parent;
 public:
     DisjointSet(int n) {
-        rank.resize(n+1, 0);
+        size.resize(n+1, 0);
         parent.resize(n+1);
         for (int i = 0; i<=n; i++) {
             parent[i] = i;
@@ -15,19 +15,17 @@ public:
         return parent[node] = findUPar(parent[node]);
     }
 
-    void unionByRank(int u, int v) {
+    void unionBySize(int u, int v) {
         int ulp_u = findUPar(u);
         int ulp_v = findUPar(v);
         if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
+        if (size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
+            size[ulp_v] += size[ulp_u];
         }
         else {
-            parent[ulp_u] = ulp_v;
-            rank[ulp_u]++;
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
